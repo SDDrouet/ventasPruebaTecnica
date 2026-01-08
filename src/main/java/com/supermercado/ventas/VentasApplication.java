47f04631c6dfe839d71b02d89bf5ba453ac8d3ef
sddrouet <sddrouet@espe.edu.ec>
2026-01-07 19:23:26 -0500

    first commit

---
+++ src/main/java/com/supermercado/ventas/VentasApplication.java
@@ -0,0 +1,13 @@
+package com.supermercado.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermercado/ventas/VentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermercado.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
